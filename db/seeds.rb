87b2f5bc9fd2457425a15a6d60eb77044b689a31
mishyjari <mishyjari@outlook.com>
2020-05-04 12:34:25 -0400

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,109 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+Category.destroy_all
+Comment.destroy_all
+Reply.destroy_all
+UserFavoritePost.destroy_all
+UserPostVote.destroy_all
+UserFavoriteCategory.destroy_all
+UserCommentVote.destroy_all
+
+# Make some users
+
+25.times do
+  User.create(
+    username: Faker::Internet.username,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.safe_email,
+    profile_img_url: Faker::LoremFlickr.image(size: '200x300'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2),
+    password: Faker::Internet.password
+  )
+end
+
+# Make some categories
+
+10.times do
+  Category.create(
+    name: Faker::Hacker.adjective,
+    user_created_id: User.all.sample.id
+  )
+end
+
+# Make some posts
+
+Category.all.each do |c|
+  25.times do
+    Post.create(
+      category_id: c.id,
+      user_id: User.all.sample.id,
+      title: Faker::Hacker.ingverb + ' ' + Faker::Hacker.noun + ' ' + Faker::Hacker.verb,
+      content: 100.times do Faker::Hacker.say_something_smart + ' ' end
+      )
+  end
+end
+
+# Make some comments
+
+Post.all.each do |p|
+  5.times do
+    Comment.create(
+      post_id: p.id,
+      user_id: User.all.sample.id,
+      content: Faker::Lorem.paragraph(sentence_count: 4)
+    )
+  end
+end
+
+# Make some replies
+Comment.all.each do |c|
+  Reply.create(
+    parent_id: c.id,
+    user_id: User.all.sample.id,
+    content: Faker::Hacker.say_something_smart
+  )
+end
+
+# Gives posts some user votes
+Post.all.each do |p|
+  25.times do
+    UserPostVote.create(
+      post_id: p.id,
+      user_id: User.all.sample.id,
+      up_vote?: Faker::Boolean.boolean(true_ratio: 0.8)
+    )
+  end
+end
+
+# Give users some favorited posts
+User.all.each do |u|
+  10.times do 
+    UserFavoritePost.create(
+      user_id: u.id,
+      post_id: Post.all.sample.id
+    )
+  end
+end
+
+# Give users some favorite categories
+User.all.each do |u|
+  3.times do 
+    UserFavoriteCategory.create(
+      user_id: u.id,
+      category_id: Category.all.sample.id
+    )
+  end
+end
+
+# Give the comments some votes
+Comment.all.each do |c|
+  20.times do
+    UserCommentVote.create(
+      comment_id: c.id,
+      user_id: User.all.sample.id,
+      up_vote?: Faker::Boolean.boolean(true_ratio: 0.8)
+    )
+  end
+end
+
