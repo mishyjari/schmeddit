ec368c606ecacdfff679a8fafa0c46bfccfdc813
theluispestana <theluispestana@gmail.com>
2020-05-04 16:46:29 -0400

    created reply associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   has_many :user_favorite_categories
   has_many :user_post_votes
   has_many :user_comment_posts
+  has_many :replies
+  has_many :parents, through: :replies
+  has_many :children, through: :replies 
 end
