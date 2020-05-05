f591a9dcb66ac4532a9a23e13d58225ea64826ae
mishyjari <mishyjari@outlook.com>
2020-05-05 10:18:11 -0400

    top three posts

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,8 @@ class Category < ApplicationRecord
   has_many :posts
   has_many :user_favorite_categories
 
-  def most_liked_post_in_category
-    self.posts.sort_posts_by_most_liked.first
+  def top_three_posts
+    self.posts.sort_posts_by_most_liked.slice(0,3)
   end
 
   def most_favorited_post_in_category
