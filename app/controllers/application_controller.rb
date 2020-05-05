949f48098982f1a5a8c8846caedfac798130b0a3
theluispestana <theluispestana@gmail.com>
2020-05-05 17:43:23 -0400

    working on signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
-  helper_method :user
-  
-  def user
+  before_action :set_user 
+
+  def set_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user
+    if !@current_user
+      redirect_to new_login_path
+    end
   end
 end
