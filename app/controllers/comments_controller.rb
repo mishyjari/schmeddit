33bc3a45610491bd631d9cf3efdc05ac55cea5a3
theluispestana <theluispestana@gmail.com>
2020-05-07 15:58:54 -0400

    improved forms

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,12 @@
 class CommentsController < ApplicationController
 
   def new
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    else
+      redirect_to login_path
+    end 
+
     @errors = flash[:err]
     if params[:parent_comment]
       @parent_comment = Comment.find(params[:parent_comment])
