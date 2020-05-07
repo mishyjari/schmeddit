3ede4baece0f261b5949b6dd2703bbed09697e1e
mishyjari <mishyjari@outlook.com>
2020-05-07 00:21:12 -0400

    categories index collapsible

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,10 +3,6 @@ class Category < ApplicationRecord
   has_many :user_favorite_categories
 
   def top_three_posts
-    self.posts.sort_posts_by_most_liked.slice(0,3)
-  end
-
-  def most_favorited_post_in_category
-    self.posts.sort_posts_by_most_favorites.first
+    self.posts.sort_by{|post| -post.score }.slice(0,3)
   end
 end
